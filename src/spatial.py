f86b0ed98f8dd0547f102d18a4986e80099d92d5
alzulueta <alzulueta@up.edu.ph>
2026-03-28 08:16:19 +0800

    Implement polymorphic method in SpatialObject

---
+++ src/spatial.py
@@ -0,0 +1,8 @@
+class SpatialObject:
+
+    def effective_area(self):
+        """
+        Return the spatial area representation of the object.
+        Subclasses must implement this behavior.
+        """
+        raise NotImplementedError
